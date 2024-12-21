5dcbed7bd4fdd7c3957c8347b94e8cfa81383c96
Rayna-Yu <Alinasharon10@gmail.com>
2024-12-21 17:09:35 -0500

    Update game.py

--- game.py
+++ game.py
@@ -3,25 +3,17 @@ from nimsum import NimSum
 
 class Game:
     def __init__(self):
-        self.people = set()
+        self.people = {}
     
     def login(self):
         while True:
             person = input("Sign in with your name:")
             if person in self.people:
-                print(f"Do you want to continue as {person}")
                 print(f"Welcome {person}!")
-                print(self.people)
+                print(self.people)#????
             else:
-                self._add_person()
-
-class People:
-    def __init__(self):
-        self.gamesPlayed = 0
-        self.high_nimsum_score = 0
-        self.high_mastermind_score = 0
+                self._add_person(person)
     
-    def __str__(self):
-        print(f"Games player: {self.gamesPlayed}")
-        print(f"High score for nimsum: {self.high_nimsum_score}")
-        print(f"High score for mastermind: {self.high_mastermind_score}")
+    def _add_person(self, person):
+        self.people[person] = [0, ] 
+        # the information for a person, 1st is games played, second high for nim, third high for mastermind
